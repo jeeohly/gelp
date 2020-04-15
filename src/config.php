c8af7d83b7c859d18bc2c841621ad33a987ea348
Gio Lee <giolee8@gmail.com>
2020-04-15 17:43:48 -0400

    initial commit

---
+++ src/config.php
@@ -0,0 +1,8 @@
+<?php
+$user = 'root';
+$pass = '';
+$db = 'webpl'; 
+
+$con = new mysqli('localhost', $user, $pass, $db) or die("Unable to connect"); 
+
+?>
---
+++ src/login.php
@@ -0,0 +1,28 @@
+<?php
+ session_start(); // Check connection
+ include_once("./config.php"); // To connect to the database
+ if (mysqli_connect_errno())
+ {
+ echo "Failed to connect to MySQL: " . mysqli_connect_error();
+ }
+ // Form the SQL query (an INSERT query)
+ $sql= "SELECT id FROM Project_User WHERE username='$_POST[Username]' AND password='$_POST[Password]'";
+ if (!mysqli_query($con,$sql))
+  {
+  die('Error: ' . mysqli_error($con));
+  }
+ $result = mysqli_query($con, $sql);
+ $rows = mysqli_num_rows($result);
+ $one = mysqli_fetch_array($result)[0];
+
+$_SESSION['loggedIn'] = 66; 
+ if($rows == 1){
+   echo "Logged in"; // Output to user
+   $_SESSION['loggedIn'] = $one;
+   header('Location: /cs4640/index.php');   
+ }
+ else{
+   echo "Invalid username or password";
+ }
+ mysqli_close($con);
+?>
